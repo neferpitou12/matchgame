068872519d7139d3c08042b8b39497eea01b917a
Manon D <m.dalh@live.nl>
2018-10-20 13:41:28 +0200

    fix values bug

--- project/resources/js/match-game.js
+++ project/resources/js/match-game.js
@@ -1,31 +1,36 @@
-$(document).ready(function() {
-
-
 var MatchGame = {};
-
 /*
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
 */
+$(document).ready(function() {
+  var cardValues = MatchGame.generateCardValues();
+  var $game = $('#game');
+  MatchGame.renderCards(cardValues, $game);
+});
 
 /*
   Generates and returns an array of matching card values.
  */
 MatchGame.generateCardValues = function () {
-var values = [];
-for (i=1; i<9; i++) {
-  values.push(i),
-  values.push(i)
-};
+  var values = [];
 
-var cardValues = [];
-while (0 < values.length) {
-  var index = Math.floor(Math.random() * (values.length));
-  cardValues.push(values[index])
-  values.splice(index, 1)
-}
+  for (i=1; i<9; i++) {
+    values.push(i);
+    values.push(i);
+  }
 
-return cardValues
+  return values;
+
+  var cardValues = [];
+
+  while (0 < values.length) {
+    var index = Math.floor(Math.random() * (values.length));
+    var random = values.splice(index, 1);
+    cardValues.push(values[index]);
+  }
+
+  return cardValues;
 };
 
 /*
@@ -34,26 +39,34 @@ return cardValues
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
-var colors = ['hsl(25, 85%, 65%)', 'hsl(55, 85%, 65%)', 'hsl(90, 85%, 65%)', 'hsl(160, 85%, 65%)',
-'hsl(220, 85%, 65%)', 'hsl(265, 85%, 65%)','hsl(310, 85%, 65%)','hsl(360, 85%, 65%)','hsl(25, 85%, 65%)']
-$game.empty();
-for (index in random) {
-  var $card = $('<div class="col-xs-3 card"></div>')
-  $card.data('value', cardValues[index])
-  $card.data('flip', false)
-  $card.data('color', colors[(cardValues[index] - 1)])
-  $card.append($game)
+  $game.data() = [];
+
+  var colors = ['hsl(25, 85%, 65%)', 'hsl(55, 85%, 65%)', 'hsl(90, 85%, 65%)', 'hsl(160, 85%, 65%)',
+  'hsl(220, 85%, 65%)', 'hsl(265, 85%, 65%)','hsl(310, 85%, 65%)','hsl(360, 85%, 65%)','hsl(25, 85%, 65%)'];
+
+  $game.empty();
+
+  for (var index = 0; index < cardValues.length; index++) {
+    var value = cardValues[index]
+    var color = colors[(cardValues[index] - 1)]
+    var data = {
+      value: value,
+      color: color,
+      flip: false
+    };
+
+  var $card = $('<div class="col-xs-3 card"></div>');
+  $card.data(data);
+  $game.append($card);
 }
 };
 
 
-MatchGame.renderCards(cardValues, $game)
 /*
   Flips over a given card and checks to see if two cards are flipped over.
   Updates styles on flipped cards depending whether they are a match or not.
  */
 
 MatchGame.flipCard = function($card, $game) {
-
+  
 };
-});
